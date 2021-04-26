8775c8f4bbe8da780fa5e998a9a2e394ad2fc3d5
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-26 14:31:59 -0500

    toggle function component useState

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 
 class AppClass extends Component {
   state ={
-    count: 0
+    count: 0,
+    isOn: false,
   }
 
 incrementCount = () => {
   this.setState(prevState => ({
     count: prevState.count + 1
   }));
+}
+
+
+
+toggleLight = () => {
+  this.setState(prevState => ({
+    isOn: !prevState.isOn
+  }))
 }
 
   render() {
@@ -19,13 +28,14 @@ incrementCount = () => {
       I was clicked {this.state.count} times
       </button>
       <h2>Toogle Light</h2>
-      <div style={{
+      <div 
+        style={{
         height: "50px",
         width: "50px",
-        background: "grey",
-      }}>
-
-      </div>
+        background: this.state.isOn ? "yellow" : "grey",
+      }}
+      onClick={this.toggleLight}
+      />
       </>
     )
   }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -2,14 +2,41 @@ import React, {useState} from 'react'
 
 const AppFunction = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false)
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)
   }
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn)
+  }
+
+
   return (
+    <>
+    <h2>Functional Counter</h2>
     <button onClick={incrementCount}>
       I was clicked {count} times with useState
     </button>
+    <h2>Toogle Light</h2>
+      <img
+        src={
+          isOn
+          ? "https://icon.now.sh/highlight/fd0"
+          : "https://icon.now.sh/highlight/aaa"
+        } 
+        style={{
+        height: "50px",
+        width: "50px",
+        
+      }}
+      alt="Flashlight"
+      onClick={toggleLight}
+      />
+
+
+
+    </>
   )
 }
 
